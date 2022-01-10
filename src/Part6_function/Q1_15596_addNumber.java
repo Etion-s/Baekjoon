619fae15913ebb7548f13d61762069569375a6b8
KwackbackE <ehdgur0591@gmail.com>
2022-01-10 13:21:03 +0900

    Start Part6_function

---
+++ src/Part6_function/Q1_15596_addNumber.java
@@ -0,0 +1,19 @@
+package Part6_function;
+
+import java.util.Scanner;
+
+public class Q1_15596_addNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        long result = 0;
+
+        for (int i=0; i<n; i++){
+            a[i] = n+1;
+        }
+        
+    }
+
+}
