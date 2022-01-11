d2c72e8773f2798aea265ffb4df0b4d5db705bb2
KwackbackE <ehdgur0591@gmail.com>
2022-01-11 12:14:48 +0900

    Create Part7 & finsh Part7_Q1_ASCII

---
+++ src/Part7_String/Q1_11654_ASCII.java
@@ -0,0 +1,15 @@
+package Part7_String;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q1_11654_ASCII {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int ch = sc.next().charAt(0);
+        System.out.println(ch);
+
+    }
+}
