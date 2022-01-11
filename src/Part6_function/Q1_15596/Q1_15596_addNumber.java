7794957b5d3031861c379cbf7823d55d1400be29
KwackbackE <ehdgur0591@gmail.com>
2022-01-11 11:48:14 +0900

    Finsh Part6_Q15596

---
+++ src/Part6_function/Q1_15596/Q1_15596_addNumber.java
@@ -0,0 +1,22 @@
+package Part6_function.Q1_15596;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q1_15596_addNumber {
+    int sum(int x, int y){
+        return x+y;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Q1_15596_addNumber q1 = new Q1_15596_addNumber();
+        int N = sc.nextInt();
+        int[] a = new int[N];
+
+        q1.sum(1,2);
+
+
+    }
+}
+
+
